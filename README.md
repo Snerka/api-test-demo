7c8788fb13986a5fe6ee2ce5a0f867481b39a05b
Snerka <v.kasner.mk@gmail.com>
2024-10-08 19:23:07 +0200

    README Update + constants adjustement

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Dhimahi employment opportunity test task for Valentino Kašner
-
 ## Challenge
 
 Based on API specification at https://api.practicesoftwaretesting.com/api/documentation, focus on `/brands` endpoints and write the following automation tests:
@@ -9,9 +7,3 @@ Based on API specification at https://api.practicesoftwaretesting.com/api/docume
 
 ## Automation framework and language
 * Java, TestNG, Maven, RestAssured, Allure, Docker
-
-## Bonus points
-* Well designed automation test strategy
-* Well structured code, following language-specific coding methods
-* Automatically generated test report
-* Usage of Docker
--- src/test/java/com/dhimahi/apitest/TestConstants.java
+++ src/test/java/com/dhimahi/apitest/TestConstants.java
@@ -5,8 +5,8 @@ public class TestConstants {
     public static final String TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5wcmFjdGljZXNvZnR3YXJldGVzdGluZy5jb20vdXNlcnMvbG9naW4iLCJpYXQiOjE3Mjc5OTY4NzAsImV4cCI6MTcyNzk5NzE3MCwibmJmIjoxNzI3OTk2ODcwLCJqdGkiOiJRUUExWXg0c1l1OWE5R1lrIiwic3ViIjoiMDFKOUE5U0Y0V0ZORTRIRU02MUdYUTAwRzgiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3Iiwicm9sZSI6ImFkbWluIn0.StmJW5wNnXuNoiCRI9mLJpqD5SN7r2P7QDR1nAju1oU"; // API token
 
     // JSON values Constants
-    public static final String NEW_BRAND_NAME = "DhiMahi Giga Chad"; // New brand name
-    public static final String NEW_BRAND_SLUG = "dhi-mahi-giga-chad"; // New brand slug
+    public static final String NEW_BRAND_NAME = "Giga Chad Brand"; // New brand name
+    public static final String NEW_BRAND_SLUG = "giga-chad"; // New brand slug
     public static final String UPDATED_BRAND_NAME = "Updated Giga Chad"; // Updated brand name
 
     // JSON Body Templates
